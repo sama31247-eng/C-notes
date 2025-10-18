757e23e265d4350f166298886b286ae0193caf54
sambhav <sama31247@gmail.com>
2025-10-18 22:58:19 +0530

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+int main(){
+    //thia is how you use print statement//
+
+    std::cout <<"my name is sambhav";  // if the code is correct it will return 0 else if 1 if there is an error//
+    std::cout <<"i am doing b.tech" <<'\n';
+    
+
+    //if you want to print code of two print in different lines then use <<'/n'at end //
+           
+    std::cout <<"my name is sambhav" << '\n';
+    std::cout <<"my brach is cs"<<'\n' ;
+    
+    /*this
+    is 
+    a muli line comment*/
+    
+    // how to use varibles//
+
+    int x;           //this is to tell that c is integer//
+    x=5;            //this is to assign the value to the x varible//
+    std::cout << x ;
+    
+    // how to sum//
+
+    int x=5;
+    int y=6;
+    int sum= x+y;
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum;
+
+    //data types//
+
+    //int//
+
+    int age=15;
+    int day=24;
+    int year=2025;
+    std::cout << age <<'\n';     // this data type can only be used for whole number//
+    std::cout << day <<'\n';      
+    std::cout << year << '\n';
+
+    // double // 
+
+    double money =9.88;
+    double gpa =8.5;      // this data type is used for decimal number//
+    double deci =1.5;
+
+    std::cout << money <<'\n';
+    std::cout << gpa <<'\n';
+    std::cout << deci <<'\n';
+    return 0;
+    
+
+
+
+
+
+
+    
+    
+}
